847edd5b3ab497c510d5c2519ae3253f733b43a7
Akramtello <35160721+akram-tello@users.noreply.github.com>
2021-12-14 09:22:43 +0800

    added kitarcar-home route

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/kitcar', function () {
+    return view('kitcar-home');
+});
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
